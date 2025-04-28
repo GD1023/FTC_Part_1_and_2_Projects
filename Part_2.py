4f673101f6a6eec76e8c344efdfe66d6fd5afc5f
GD1023 <geetanshdhuria@gmail.com>
2025-04-27 20:42:21 -0700

    Initial commit

---
+++ Part_2.py
@@ -0,0 +1,76 @@
+import pygame
+import sys
+
+BLACK = (0, 0, 0)
+BLUE= (0,0,255)
+YELLOW= (255,255,0)
+
+# Create the starting screen
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Falling Sand")
+grid=[]
+# Here I am creating a grid with 800 rows and 800 columns
+
+for a in range(80):
+    row = []
+    for b in range(80):
+        row.append(0)
+    grid.append(row)
+
+def draw_square(x,y,val):
+    if val == 1: # Water box being created
+        rectangle = pygame.Rect(x * 10, y * 10, 10, 10)
+        pygame.draw.rect(screen, BLUE, rectangle)
+    elif val==2: # Sand box being created
+        rectangle = pygame.Rect(x * 10, y * 10, 10, 10)
+        pygame.draw.rect(screen, YELLOW, rectangle)
+    else:
+        rectangle = pygame.Rect(x * 10, y * 10, 10, 10)
+        pygame.draw.rect(screen, BLACK, rectangle)
+
+
+def update_screen():
+    for y in range(78,-1,-1):
+        for x in range(80):
+            if grid[y][x] == 2:  # Sand
+                if grid[y + 1][x] == 0: #Sand falling down
+                    grid[y][x], grid[y + 1][x] = 0, 2
+                elif grid[y+1][x] == 1:
+                    grid[y+1][x],grid[y][x] = 2,1
+                elif x > 0 and grid[y + 1][x - 1] == 0:
+                    grid[y][x], grid[y + 1][x - 1] = 0, 2
+                elif x < 79 and grid[y + 1][x + 1] == 0:
+                    grid[y][x], grid[y + 1][x + 1] = 0, 2
+                else:
+                    grid[y][x]=2
+
+            if grid[y][x] == 1:  # Water
+                if grid[y + 1][x] == 0:
+                    grid[y][x], grid[y + 1][x] = 0, 1
+                else:
+                    if x > 0 and grid[y][x - 1] == 0:
+                        grid[y][x], grid[y][x - 1] = 0, 1
+                    elif x < 79 and grid[y][x + 1] == 0:
+                        grid[y][x], grid[y][x + 1] = 0, 1
+
+while True:
+    # this is just a constant loop listening for user inputs
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if pygame.mouse.get_pressed()[0]:  # THis is for left click
+            mousex, mousey = pygame.mouse.get_pos()
+            grid[(mousey // 10)%80][(mousex // 10)%80] = 1
+
+        if pygame.mouse.get_pressed()[2]:
+            mousex, mousey = pygame.mouse.get_pos()
+            grid[(mousey // 10)%80][(mousex // 10)%80] = 2
+
+    for y in range(80):
+        for x in range(80):
+            draw_square(x, y, grid[y][x])
+    update_screen()
+    pygame.display.update()
---
+++ main.py
@@ -0,0 +1,109 @@
+import pygame
+import sys
+
+# constants to remember
+
+# So the screen size is 800 by 800,
+# This code is written in such a way that you can change the numebr of columns and rows, AS LONG AS WHEN THEY ARE MULTIPLED BY THE CELL SIZE, you get 800
+# For Example, if you want 80 rows and 80 columns, make the cell_size 10 or you can have 160 rows and columns with the cell size being 5
+# The reason for this is because otherwise it goes out of the index
+ROWS, COLS = 160,160
+CELL_SIZE=5
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Create the starting screen
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Conway's Game of Life")
+
+
+grid=[]
+# Here I am creating a grid with 800 rows and 800 columns
+for a in range(ROWS):
+    row = []
+    for b in range(COLS):
+        row.append(0)
+    grid.append(row)
+
+running = False
+# Everytime the simulation completes the conditions, the screen is updated accordingly using this function( Just turning the squared either black or white)
+def update_screen():
+    screen.fill(BLACK)
+    for y in range(ROWS):
+        for x in range(COLS):
+            if grid[y][x] == 1:
+                rectangle = pygame.Rect(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE)
+                pygame.draw.rect(screen, WHITE, rectangle)
+
+
+def neighbors_sum(grid, row, col):
+    neighbors=0
+    for horizontal_shift in range (-1,2):
+        for vertical_shift in range(-1,2):
+            if horizontal_shift == 0 and vertical_shift == 0:
+                continue
+            else:
+                r_cor= (row+vertical_shift)
+                c_cor= (col+horizontal_shift)
+            if 0 <= r_cor < ROWS and 0 <= c_cor < COLS:
+                neighbors += grid[r_cor][c_cor]
+    return neighbors
+
+while True:
+    # this is just a constant loop listening for user inputs
+    for event in pygame.event.get():
+        if event.type== pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if pygame.mouse.get_pressed()[0]: # THis is for left click
+            mousex, mousey= pygame.mouse.get_pos()
+            grid[mousey//CELL_SIZE][mousex//CELL_SIZE]=1
+
+        if pygame.mouse.get_pressed()[2]:
+            mousex,mousey= pygame.mouse.get_pos()
+            grid[mousey // CELL_SIZE][mousex//CELL_SIZE]=0
+            rectangle_1 = pygame.Rect(mousex*CELL_SIZE, mousey*CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(screen, BLACK, rectangle_1)
+
+        if event.type== pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                running = not running    # The reason why we use not running, rather than just flipping running to True has to do with the fact that if we click the space bar again, the code should stop, thus running shoul;d be the opposite of what it was originally.
+
+
+   # once the user clicks the space bar to start the program,  this is initiated.
+    if running:
+        new_grid=[]
+        for a in range(ROWS):
+            row = []
+            for b in range(COLS):
+                row.append(0)
+            new_grid.append(row)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                # using the function we previously defined to evaluate whether the cell dies or stays alive
+                neighbors= neighbors_sum(grid, r, c)
+                if grid[r][c] ==1:
+                    if neighbors<2 or neighbors>3:
+                        new_grid[r][c]=0
+                    else:
+                        new_grid[r][c]=1
+                else:
+                    if neighbors==3:
+                        new_grid[r][c]=1
+        # Once the evaluation is complete, everything is updated onto the screen
+        grid=new_grid
+    update_screen()
+    pygame.display.update()
+
+
+
+
+
+
+
+
+
+
